57a933f45560c59f241296015ee8a055d97068c8
jrcntt <jrcntt@gmail.com>
2025-04-21 07:41:56 +0700

    UPDATE

--- mywebsite/home/views.py
+++ mywebsite/home/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib.auth.models import User
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 # rooms =[
 #     {'id': 1, 'name':'Lets learn python!'},
@@ -16,8 +17,13 @@ from django.contrib.auth.decorators import login_required
 
 
 def loginPage(request):
+
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('index')
+
     if request.method =='POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -32,13 +38,27 @@ def loginPage(request):
         else:
             messages.error(request, "Username or password does not exist")
 
-    context = {}
+    context = {'page':page}
     return render(request, 'home/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('index')
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username =user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'An error accurred during registration')
+    return render(request, 'home/login_register.html',{'form':form})
 
 
 def index(request):
@@ -57,7 +77,18 @@ def index(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}      
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+
+        )
+        return redirect('room', pk=room.id)
+
+    context = {'room':room, 'room_messages': room_messages}      
     return render(request,'home/room.html',context)
 
 
